e6952bcc93cac09dbf29a8a2ec9743e610812cec
chyngyz-kydraliev <cingizkydraliev@gmail.com>
2023-12-25 10:12:01 +0600

    Initial commit

---
+++ src/main/java/com/kydraliev/walletManager/WalletManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kydraliev.walletManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kydraliev/walletManager/controllers/GreetingsController.java
@@ -0,0 +1,14 @@
+package com.kydraliev.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/api/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue = "World") String name) {
+        return "Hello" + name + "!";
+    }
+
+}
